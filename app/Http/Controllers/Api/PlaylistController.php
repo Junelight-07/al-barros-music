228f7dbd70dc08f3ad54cef36b018c1313cc29bc
alexy <alexy.de.barros@futurmaster.com>
2025-05-21 17:17:01 +0200

    add api keys

---
+++ app/Http/Controllers/Api/PlaylistController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlaylistController extends Controller
+{
+    /**
+     * Récupérer toutes les playlists de l'utilisateur.
+     */
+    public function index(Request $request)
+    {
+        if ($request->has('user_from_api_key')) {
+            $user = $request->user_from_api_key;
+            $playlists = $user->playlists()->with('tracks')->get();
+
+            return response()->json([
+                'playlists' => $playlists
+            ]);
+        }
+    }
+}
---
+++ app/Http/Middleware/ApiKeyMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+        namespace App\Http\Middleware;
+
+        use App\Models\ApiKey;
+        use Closure;
+        use Illuminate\Http\Request;
+        use Symfony\Component\HttpFoundation\Response;
+
+        class ApiKeyMiddleware
+        {
+            /**
+             * Handle an incoming request.
+             */
+            public function handle(Request $request, Closure $next): Response
+            {
+                $apiKey = $request->header('x-api-key');
+
+                if (!$apiKey) {
+                    return response()->json(['error' => 'Clé API manquante'], 401);
+                }
+
+                $apiKeyModel = ApiKey::where('key', $apiKey)->first();
+
+                if (!$apiKeyModel) {
+                    return response()->json(['error' => 'Clé API invalide'], 401);
+                }
+
+                // Associer l'utilisateur à la requête avec user_from_api_key au lieu de user
+                $request->merge(['user_from_api_key' => $apiKeyModel->user]);
+
+                return $next($request);
+            }
+        }
